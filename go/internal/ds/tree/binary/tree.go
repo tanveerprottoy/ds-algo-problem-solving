df175cb6b541881b85f9b2b686bcef81410f5656
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-05-26 23:33:38 +0600

    add new ds

--- go/internal/ds/tree/binary/tree.go
+++ go/internal/ds/tree/binary/tree.go
@@ -6,8 +6,11 @@ type Node[T any] struct {
 	rightChild *Node[T]
 }
 
+func NewNode[T any](data T, leftChild, rightChild *Node[T]) *Node[T] {
+	return &Node[T]{data: data, leftChild: leftChild, rightChild: rightChild}
+}
+
 /*
-Create – create a tree in data structure.
 Insert − Inserts data in a tree.
 Search − Searches specific data  in a tree to check it is present or not.
 Preorder Traversal – perform Traveling a tree in a pre-order manner in data structure .
@@ -15,6 +18,5 @@ In order Traversal – perform Traveling a tree in an in-order manner.
 Post order Traversal –perform Traveling a tree in a post-order manner.
 */
 type Tree[T any] struct {
-	Node T
-	Edge *T
+	Root *Node[T]
 }
