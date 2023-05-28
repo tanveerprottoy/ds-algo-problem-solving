8b1f546a56bb105a397da4431a33c4ed2465b741
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-05-28 15:24:37 +0600

    add new ds

--- go/internal/ds/tree/binary/tree_alt.go
+++ go/internal/ds/tree/binary/tree_alt.go
@@ -3,6 +3,7 @@ package binary
 import (
 	"fmt"
 
+	"github.com/tanveerprottoy/ds-algo-problem-solving/internal/ds/stack"
 	"golang.org/x/exp/constraints"
 )
 
@@ -49,7 +50,31 @@ func (t *TreeAlt[T]) InorderTraverse() {
 	
 }
 
+/*
+
+Create an empty stack
+Initialize the current node as root.
+Push the current node to S and set current = current->left 
+until current is NULL
+If current is NULL and the stack is not empty then:
+Pop the top item from the stack.
+Print the popped item and set current = popped_item->right 
+Go to step 3.
+If current is NULL and the stack is empty then we are done.
+
+Start from the root, call it PTR.
+Push PTR onto stack if PTR is not NULL.
+Move to left of PTR and repeat step 2.
+If PTR is NULL and stack is not empty, 
+then Pop element from stack and set as PTR.
+Process PTR and move to right of PTR , go to step 2.
+*/
 func (t *TreeAlt[T]) InorderTraverseStack() {
+	s := stack.
+	r := t.Root
+	if r != nil {
+
+	}
 	fmt.Println("InorderTraverseStack")
 }
 
