d00bedbd3151e69b98ee587f1381752485c508c3
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-05-31 22:25:09 +0600

    add new code

--- go/internal/ds/tree/binary/tree.go
+++ go/internal/ds/tree/binary/tree.go
@@ -226,13 +226,48 @@ func (t *Tree[T]) PostorderTraverse(n *Node[T]) {
 */
 func (t *Tree[T]) PostorderTraverseStack() {
 	s := stack.NewStack[*Node[T]]()
-	curr := t.Root
-	for curr != nil {
-		if curr.right != nil {
-			s.Push(curr.right)
-		}
-		curr = curr.left
-	}
+	if (node == null)
+            return list;
+        S.push(node);
+        Node prev = null;
+        while (!S.isEmpty()) {
+            Node current = S.peek();
+ 
+            /* go down the tree in search of a leaf an if so
+            process it and pop stack otherwise move down */
+            if (prev == null || prev.left == current || prev.right == current) {
+                if (current.left != null)
+                    S.push(current.left);
+                else if (current.right != null)
+                    S.push(current.right);
+                else {
+                    S.pop();
+                    list.add(current.data);
+                }
+ 
+                /* go up the tree from left node, if the
+                child is right push it onto stack otherwise
+                process parent and pop stack */
+            }
+            else if (current.left == prev) {
+                if (current.right != null)
+                    S.push(current.right);
+                else {
+                    S.pop();
+                    list.add(current.data);
+                }
+ 
+                /* go up the tree from right node and after
+                coming back from right node process parent
+                and pop stack */
+            }
+            else if (current.right == prev) {
+                S.pop();
+                list.add(current.data);
+            }
+ 
+            prev = current;
+        }
 }
 
 /*
