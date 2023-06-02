5392597dabdf9e85a01caccf5d897826b92b7262
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-06-02 23:36:01 +0600

    add new ds algos

--- go/internal/ds/tree/binary/tree.go
+++ go/internal/ds/tree/binary/tree.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/tanveerprottoy/ds-algo-problem-solving/internal/ds/stack"
+	"github.com/tanveerprottoy/ds-algo-problem-solving/pkg/slice"
 	"golang.org/x/exp/constraints"
 )
 
@@ -58,9 +59,9 @@ func (t *Tree[T]) InorderTraverse(n *Node[T]) {
 
 /*
 1 Start from the root.
-2 Push curr onto stack if curr is not NULL.
+2 Push curr onto stack if curr is not nil.
 3 Move to left of curr and repeat step 2.
-4 If curr is NULL and stack is not empty, then Pop element from stack and set as curr.
+4 If curr is nil and stack is not empty, then Pop element from stack and set as curr.
 5 Process curr and move to right of curr , go to step 2
 */
 func (t *Tree[T]) InorderTraverseStack(n *Node[T]) {
@@ -89,11 +90,11 @@ func (t *Tree[T]) InorderTraverseStack(n *Node[T]) {
 /*
 Inorder traversal using Morris Traversal:
 Initialize the current node as root.
-While current is not null, check if it has a left child.
+While current is not nil, check if it has a left child.
 If there is no left child, print the current node and move to the right child of the current node.
 Otherwise, find the rightmost node of the left subtree or the node whose right child is the current node:
-If the right child is NULL, make current as the right child and move to the left child of current.
-If the right child is the current node itself, print current node, make the right child NULL and move to the right child of the current node.
+If the right child is nil, make current as the right child and move to the left child of current.
+If the right child is the current node itself, print current node, make the right child nil and move to the right child of the current node.
 */
 func (t *Tree[T]) InorderTraverseMorris() {
 	curr := t.Root
@@ -108,7 +109,7 @@ func (t *Tree[T]) InorderTraverseMorris() {
 			// left node exists
 			tmp := curr.left
 			for tmp.right != nil && tmp.right != curr {
-				// iterate until right = null & right == curr
+				// iterate until right = nil & right == curr
 				// go to right
 				tmp = tmp.right
 			}
@@ -165,16 +166,16 @@ func (t *Tree[T]) PreorderTraverseStack() {
 
 /*
 Morris traversal
-1...If left child is null, print the current node data. Move to right child.
+1...If left child is nil, print the current node data. Move to right child.
 ….Else, Make the right child of the inorder predecessor point to the current node. Two cases arise:
-………a) The right child of the inorder predecessor already points to the current node. Set right child to NULL. Move to right child of current node.
-………b) The right child is NULL. Set it to the current node. Print the current node’s data and move to left child of current node.
-2...Iterate until the current node is not NULL.
+………a) The right child of the inorder predecessor already points to the current node. Set right child to nil. Move to right child of current node.
+………b) The right child is nil. Set it to the current node. Print the current node’s data and move to left child of current node.
+2...Iterate until the current node is not nil.
 */
 func (t *Tree[T]) PreorderTraverseMoris() {
 	curr := t.Root
 	for curr != nil {
-		// If left child is null, print the current node data. Move to
+		// If left child is nil, print the current node data. Move to
 		// right child.
 		if curr.left == nil {
 			fmt.Println(curr.val)
@@ -214,87 +215,157 @@ func (t *Tree[T]) PostorderTraverse(n *Node[T]) {
 
 /*
 1.1 Create an empty stack
-2.1 Do following while root is not NULL
-    a) Push root's right child and then root to stack.
-    b) Set root as root's left child.
+2.1 Do following while root is not nil
+
+	a) Push root's right child and then root to stack.
+	b) Set root as root's left child.
+
 2.2 Pop an item from stack and set it as root.
-    a) If the popped item has a right child and the right child 
-       is at top of stack, then remove the right child from stack,
-       push the root back and set root as root's right child.
-    b) Else print root's data and set root as NULL.
+
+	a) If the popped item has a right child and the right child
+	   is at top of stack, then remove the right child from stack,
+	   push the root back and set root as root's right child.
+	b) Else print root's data and set root as nil.
+
 2.3 Repeat steps 2.1 and 2.2 while stack is not empty.
 */
-func (t *Tree[T]) PostorderTraverseStack() {
+func (t *Tree[T]) PostorderTraverse1Stack() {
+	// Check for empty tree
+	if t.Root == nil {
+		return
+	}
 	s := stack.NewStack[*Node[T]]()
-	if (node == null)
-            return list;
-        S.push(node);
-        Node prev = null;
-        while (!S.isEmpty()) {
-            Node current = S.peek();
- 
-            /* go down the tree in search of a leaf an if so
-            process it and pop stack otherwise move down */
-            if (prev == null || prev.left == current || prev.right == current) {
-                if (current.left != null)
-                    S.push(current.left);
-                else if (current.right != null)
-                    S.push(current.right);
-                else {
-                    S.pop();
-                    list.add(current.data);
-                }
- 
-                /* go up the tree from left node, if the
-                child is right push it onto stack otherwise
-                process parent and pop stack */
-            }
-            else if (current.left == prev) {
-                if (current.right != null)
-                    S.push(current.right);
-                else {
-                    S.pop();
-                    list.add(current.data);
-                }
- 
-                /* go up the tree from right node and after
-                coming back from right node process parent
-                and pop stack */
-            }
-            else if (current.right == prev) {
-                S.pop();
-                list.add(current.data);
-            }
- 
-            prev = current;
-        }
+	var data []*Node[T]
+	var prev *Node[T]
+	s.Push(t.Root)
+	for !s.IsEmpty() {
+		current := s.Peek()
+		/* go down the tree in search of a leaf an if so
+		   process it and pop stack otherwise move down */
+		if prev == nil || prev.left == current || prev.right == current {
+			if current.left != nil {
+				s.Push(current.left)
+			} else if current.right != nil {
+				s.Push(current.right)
+			} else {
+				s.PopAlt()
+				data = append(data, current)
+			}
+			/* go up the tree from left node, if the
+			   child is right push it onto stack otherwise
+			   process parent and pop stack */
+		} else if current.left == prev {
+			if current.right != nil {
+				s.Push(current.right)
+			} else {
+				s.PopAlt()
+				data = append(data, current)
+			}
+			/* go up the tree from right node and after
+			   coming back from right node process parent
+			   and pop stack */
+		} else if current.right == prev {
+			s.PopAlt()
+			data = append(data, current)
+		}
+		prev = current
+	}
+	for _, e := range data {
+		fmt.Println(e.val)
+	}
 }
 
 /*
- */
+ 1. Push root to first stack.
+ 2. Loop while first stack is not empty
+    2.1 Pop a node from first stack and push it to second stack
+    2.2 Push left and right children of the popped node to first stack
+ 3. Print contents of second stack
+*/
+func (t *Tree[T]) PostorderTraverse2Stacks() {
+	s1 := stack.NewStack[*Node[T]]()
+	s2 := stack.NewStack[*Node[T]]()
+	if t.Root == nil {
+		return
+	}
+	// push root to first stack
+	s1.Push(t.Root)
+	// Run while first stack is not empty
+	for !s1.IsEmpty() {
+		// Pop an item from s1 and push it to s2
+		temp := s1.PopAlt()
+		s2.Push(temp)
+		// Push left and right children of
+		// removed item to s1
+		if temp.left != nil {
+			s1.Push(temp.left)
+		}
+		if temp.right != nil {
+			s1.Push(temp.right)
+		}
+	}
+	// Print all elements of second stack
+	for !s2.IsEmpty() {
+		temp := s2.PopAlt()
+		fmt.Println(temp.val)
+	}
+}
+
+/*
+Create a slice and Initialize current as root
+While current is not nil
+If the current does not have a right child
+push the current key in slice
+
+	Go to the left, i.e., current = current->left
+
+else
+Find leftmost node in current right subtree OR node whose left child == current.
+if current  does not have a left child
+push the current node in the slice
+Make current as of the left child of that leftmost node
+Go to this right child, i.e., current = current->right
+else
+Found left child == current
+Update the left child as nil of that node whose left child is current
+Go to the left, i.e. current = current->left
+
+In the last, we reverse the slice and print it, since slice is used to store the output, the space complexity of this algorithm would be O(N).
+*/
 func (t *Tree[T]) PostorderTraverseMoris() {
-	s := stack.NewStack[*Node[T]]()
-	curr := t.Root
-	for curr != nil {
-		if curr.right != nil {
-			s.Push(curr)
-			curr = curr.left
+	var data []*Node[T]
+	current := t.Root
+	for current != nil {
+		// If right child is nil,
+		// put the current node data
+		// in res. Move to left child.
+		if current.right == nil {
+			data = append(data, current)
+			current = current.left
 		} else {
-			prev := curr.right
-			for prev.left != nil && prev.left != curr {
-				prev = prev.left
+			predecessor := current.right
+			for predecessor.left != nil && predecessor.left != current {
+				predecessor = predecessor.left
 			}
-			if prev.left != nil {
-				prev.left = curr
-				s.Push(curr)
-				curr = curr.right
+			// If left child doesn't point
+			// to this node, then put in res
+			// this node and make left
+			// child point to this node
+			if predecessor.left == nil {
+				data = append(data, current)
+				predecessor.left = current
+				current = current.right
 			} else {
-				curr = curr.left
-				prev.left = nil
+				// If the left child of inorder predecessor
+				// already points to this node
+				predecessor.left = nil
+				current = current.left
 			}
 		}
 	}
-	for !s.IsEmpty() {
-		fmt.Println(s.PopAlt().val)
+	// reverse the res
+	slice.Reverse(data)
+	for _, e := range data {
+		fmt.Println(e.val)
 	}
 }
--- go/pkg/slice/slice.go
+++ go/pkg/slice/slice.go
@@ -1,9 +1,33 @@
 package slice
 
-func RemoveAt[T any](slice []T, index int) []T {
-	return append(slice[:index], slice[index+1:]...)
+import "reflect"
+
+/*
+The expression ~string means the set of all types whose underlying type is string.
+This includes the type string itself as well as all types declared with definitions such
+as type MyString string.
+Here S must be a slice type whose element type can be any type.
+*/
+func RemoveAt[S ~[]E, E any](s S, index int) S {
+	return append(s[:index], s[index+1:]...)
 }
 
-func AppendCopy[T any](src, dest []T) []T {
+func AppendCopy[S ~[]E, E any](src, dest S) S {
 	return append(dest, src...)
+}
+
+func Reverse[S ~[]E, E any](s S) S {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
+
+func ReverseSwap[S ~[]E, E any](s S) S {
+	n := reflect.ValueOf(s).Len()
+	swap := reflect.Swapper(s)
+	for i, j := 0, n-1; i < j; i, j = i+1, j-1 {
+		swap(i, j)
+	}
+	return s
 }
